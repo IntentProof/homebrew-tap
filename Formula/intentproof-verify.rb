e9e0ad01c6ddcc13828f1641cda604975fa4c82b
Nathan Gillett <nathan@intentproof.io>
2026-05-17 21:07:32 -0500

    Capture verifier help output in formula test
    Signed-off-by: Nathan Gillett <nathan@intentproof.io>

--- Formula/intentproof-verify.rb
+++ Formula/intentproof-verify.rb
@@ -51,6 +51,6 @@ class IntentproofVerify < Formula
   end
 
   test do
-    assert_match "Usage:", shell_output("#{bin}/intentproof-verify --help", 1)
+    assert_match "Usage:", shell_output("#{bin}/intentproof-verify --help 2>&1", 1)
   end
 end
